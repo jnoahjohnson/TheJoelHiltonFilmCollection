84750f7f624dbd7e3d55409b94dececea7e3737d
Noah Johnson <jnoahj@byu.edu>
2021-02-03 17:46:59 -0700

    First Commit

---
+++ TheJoelHiltonFilmCollection/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TheJoelHiltonFilmCollection.Models;
+
+namespace TheJoelHiltonFilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddMovie(Movie movie)
+        {
+            TempStorage.AddMovie(movie);
+            return View("Confirmation", movie);
+        }
+        public IActionResult ViewMovies()
+        {
+            return View(TempStorage.Movies.Where(movie => movie.Title!="Independence Day"));
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TheJoelHiltonFilmCollection/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheJoelHiltonFilmCollection.Models
+{
+    public class Movie
+    {
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+
+        [DisplayName("Lent To")]
+        public string LentTo { get; set; }
+        [StringLength(25, ErrorMessage ="Notes are limited to 25 characters.")]
+        public string Notes { get; set; }
+    }
+}
